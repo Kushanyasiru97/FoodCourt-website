84a5aa8195c9c8376f8a944758f77b73b5e99480
Kushan Yasiru <52574258+Kushanyasiru97@users.noreply.github.com>
2021-06-30 16:09:36 +0530

    Add files via upload

---
+++ Food_Court/login.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Login Page</title>
+	<link rel="stylesheet" type="text/css" href="css/loginstyle.css">
+</head>
+
+<body>
+	<div class="login-page">
+		<img src="" class="login-image">
+		<h1>Login Here</h1>
+		<form action="" method="post" enctype="multipart/form-data">
+			<p>User Name</p>
+			<input type="text" name="uname" placeholder="Enter your Email">
+			<p>Password</p>
+			<input type="password" name="pword" placeholder="Enter your Password">
+			<input type="submit" name="submit" value="Login">
+			<div class="row">
+				<a href="#">Forget Password</a>
+				<a href="">&nbsp;&nbsp;&nbsp;Sign Up</a>
+			</div>
+		</form>
+	</div>
+</body>
+</html>
